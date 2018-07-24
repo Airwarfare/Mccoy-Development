e927c00853c9740ebef693342f3e379b2fc203d9
Nathaniel <nathaniel.mccoy29@me.com>
2018-07-24 19:21:53 -0400

    Revert "Navigator Installation"
    This reverts commit 5697bf1f2a1a9a22303118a1c6fd56f86993c75e.

--- Mccoy.Altis.Altis/maverick/maverick_functions_master.cpp
+++ Mccoy.Altis.Altis/maverick/maverick_functions_master.cpp
@@ -1,3 +1,1 @@
 #include "talent-tree-modular\functions.cpp"
-#include "gui\functions\functions.cpp"
-#include "navigation\functions.cpp"
--- Mccoy.Altis.Altis/maverick/maverick_master.cpp
+++ Mccoy.Altis.Altis/maverick/maverick_master.cpp
@@ -1,3 +1,1 @@
 #include "talent-tree-modular\config.cpp"
-#include "gui\includes.cpp"
-#include "navigation\config.cpp"
