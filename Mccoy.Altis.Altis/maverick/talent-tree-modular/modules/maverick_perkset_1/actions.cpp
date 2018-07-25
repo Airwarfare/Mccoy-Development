a4c39c4988ad65583a6baffcb73762085f2d499e
IEthanHD <32881589+IEthanHD@users.noreply.github.com>
2018-07-25 23:28:09 +0100

    Welcome Back Loads More Actions

--- Mccoy.Altis.Altis/maverick/talent-tree-modular/modules/maverick_perkset_1/actions.cpp
+++ Mccoy.Altis.Altis/maverick/talent-tree-modular/modules/maverick_perkset_1/actions.cpp
@@ -1,1 +1,150 @@
+class VehiclePurchased {
+    expToAdd = 1;
+	message = "Vehicle Purchased";
+};
+
+class ItemProcessed {
+    expToAdd = 10;
+	message = "Item(s) Processed";
+};
+
+class hp {
+    expToAdd = 1;
+	message = "House Purchased";
+};
+
+class gut {
+    expToAdd = 1;
+	message = "Gutted Animal";
+};
+
+class repair {
+    expToAdd = 1;
+	message = "Repair Animal";
+};
+
+class caphid {
+    expToAdd = 1;
+	message = "Captured Hideout";
+};
+
+class hs {
+    expToAdd = 10;
+	message = "House Processed";
+};
+
+class ontake {
+    expToAdd = 10;
+	message = "Weapons Taken";
+};
+
+class postbail {
+    expToAdd = 10;
+	message = "posted bail";
+};
+
+class storage {
+    expToAdd = 10;
+	message = "Storage Added";
+};
+
+class VehicleLockpicked {
+    expToAdd = 5;
+	message = "Vehicle Lockpicked";
+};
+
+class ItemMined {
+    expToAdd = 5;
+	message = "Item(s) Mined";
+};
+
+class PlayerArrested {
+    expToAdd = 10;
+	message = "Player Arrested";
+};
+
+class PlayerTicketed {
+    expToAdd = 10;
+	message = "Player Ticketed";
+};
+
+class Fish {
+    expToAdd = 5;
+	message = "Fish Caught";
+};
+
+class Turtle {
+    expToAdd = 10;
+	message = "Turtle Caught";
+};
+
+class PassingTime {
+    expToAdd = 10;
+	message = "Playing The Server";
+};
+
+class ItemGathered {
+    expToAdd = 10;
+	message = "Item Gathered";
+};
+class LicensePurchased {
+    expToAdd = 10;
+	message = "License Purchased";
+};
+class ChopShop {
+    expToAdd = 10;
+	message = "Chop Shopped Vehicle";
+};
+class ClothesPurchased {
+    expToAdd = 10;
+	message = "Clothes Purchased";
+};
+class GangCreated {
+    expToAdd = 10;
+	message = "Gang Created";
+};
+class GangUpgraded {
+    expToAdd = 10;
+	message = "Gang Upgraded";
+};
+class HideoutCaptured {
+    expToAdd = 10;
+	message = "Hideout Captured";
+};
+class RobPlayer {
+    expToAdd = 10;
+	message = "Rob Player";
+};
+class RobGas {
+    expToAdd = 10;
+	message = "Rob Gas Station";
+};
+class Ticket {
+    expToAdd = 10;
+	message = "Police Action";
+};
+class Arrest {
+    expToAdd = 10;
+	message = "Police Action";
+};
+class Impound {
+    expToAdd = 10;
+	message = "Police Action";
+};
+class Seize {
+    expToAdd = 10;
+	message = "Police Action";
+};
+class Repair {
+    expToAdd = 10;
+	message = "Vehicle Repaired";
+};
+class BombPlaced {
+    expToAdd = 10;
+	message = "Bomb Placed";
+};
+class BombDefused {
+    expToAdd = 10;
+	message = "Police Action";
+};
 
